b3d4e79f26df6611f5b9a1ba37079a057b99b105
pankajkulsange <85685465+pankajkulsange@users.noreply.github.com>
2024-08-16 14:38:53 +0530

    Added BDD with TestNG

---
+++ AutomationExerciseSuite/src/test/java/StepDef/LoginStepDef.java
@@ -0,0 +1,44 @@
+package StepDef;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginStepDef {
+	static WebDriver driver;
+	@Given("User is on login page")
+	public void user_is_on_login_page() {
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://www.automationexercise.com/login");
+	}
+
+	@When("User enters valid {string} and {string}")
+	public void user_enters_valid_and(String uName, String pass) {
+	    driver.findElement(By.xpath("(//input[@type='email' and @name='email'])[1]")).sendKeys(uName);
+	    driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys(pass);
+	}
+
+	@When("Clicks on Login button")
+	public void clicks_on_login_button() {
+	   driver.findElement(By.xpath("//button[contains(text(), 'Login')]")).click();
+	}
+
+	@Then("User should navigate to homepage")
+	public void user_should_navigate_to_homepage() {
+	    driver.findElement(By.xpath("//h2[contains(text(), 'Category')]")).isDisplayed();
+	}
+
+	@Then("Close the browser")
+	public void close_the_browser() {
+	    driver.quit();
+	}
+
+}
